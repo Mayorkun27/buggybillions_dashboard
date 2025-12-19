f49a07afad97d98c88bee4af25c726c18727e91a
Azeez Mustapha <azeezmustapha380@gmail.com>
2025-12-19 17:32:08 +0100

    commit

--- src/pages/student/StudentCurriculm.jsx
+++ src/pages/student/StudentCurriculm.jsx
@@ -4,10 +4,10 @@ const StudentCurriculum = () => {
       date: "Phase 1: ",
       classes: [
         {
-          from: "9:30 AM",
-          to: "11:30 PM",
-          course: "Mobile App Development",
-          teacher: "Summence Ajayi",
+          week1: "Introduction To Ui Desing",
+          week2: "Introduction To Ux Desing",
+          week3: "Introduction To Proto",
+          week4: "Introduction To Ui Desing",
         },
       ],
     },
@@ -15,10 +15,10 @@ const StudentCurriculum = () => {
       date: "Phase 2: ",
       classes: [
         {
-          from: "9:30 AM",
-          to: "11:30 PM",
-          course: "Mobile App Development",
-          teacher: "Summence Ajayi",
+          week1: "Introduction To Ui Desing",
+          week2: "Introduction To Ui Desing",
+          week3: "Introduction To Ui Desing",
+          week4: "Introduction To Ui Desing",
         },
       ],
     },
@@ -26,32 +26,21 @@ const StudentCurriculum = () => {
       date: "Phase 3: ",
       classes: [
         {
-          from: "9:30 AM",
-          to: "11:30 PM",
-          course: "Mobile App Development",
-          teacher: "Summence Ajayi",
+          week1: "Introduction To Ui Desing",
+          week2: "Introduction To Ui Desing",
+          week3: "Introduction To Ui Desing",
+          week4: "Introduction To Ui Desing",
         },
       ],
     },
-    {
-      date: "Phase 4: ",
+    {  
+      date: "Phase4: ",
       classes: [
         {
-          from: "9:30 AM",
-          to: "11:30 PM",
-          course: "Mobile App Development",
-          teacher: "Summence Ajayi",
-        },
-      ],
-    },
-    {
-      date: "Phase 5: ",
-      classes: [
-        {
-          from: "9:30 AM",
-          to: "11:30 PM",
-          course: "Mobile App Development",
-          teacher: "Summence Ajayi",
+          week1: "Introduction To Ui Desing",
+          week2: "Introduction To Ui Desing",
+          week3: "Introduction To Ui Desing",
+          week4: "Introduction To Ui Desing",
         },
       ],
     },
@@ -70,52 +59,30 @@ const StudentCurriculum = () => {
               </h2>
 
               {day.classes.map((me, ola) => (
-                <div key={ola} className="bg-White p-4 rounded shadow mb-4">
+                <div
+                  key={ola}
+                  className="bg-White p-4 rounded shadow grid grid-cols-1 mb-4"
+                >
                   <div>
-                    <div className="flex justify-between">
-                      <div className="flex gap-[20rem]">
-                        <div className="flex flex-col gap-1">
-                          <h2 className="text-sm  text-black">From</h2>
-                          <p className="text-sm  text-black">{me.from}</p>
-                        </div>
+                    <div className="flex items-center justify-center gap-10">
+                      <div className="flex flex-col gap-1 bg-white shadow p-4">
+                        <h2 className="text-black font-bold">week 1:</h2>
+                        <p className="text-sm  text-black">{me.week1} </p>
+                      </div>
 
-                        <div className="flex flex-col gap-1">
-                          <h2 className="text-sm  text-black">To</h2>
-                          <p className="text-sm  text-black">{me.to}</p>
-                        </div>
+                      <div className="flex flex-col gap-1 bg-white shadow p-4">
+                        <h2 className="font-bold  text-black">week 2:</h2>
+                        <p className="text-sm text-black">{me.week2}</p>
+                      </div>
 
-                        <div className="flex flex-col gap-3">
-                          <div className="flex flex-col gap-1">
-                            <h2 className="text-sm  text-black">Course</h2>
-                            <p className="text-sm  text-black">{me.course}</p>
-                          </div>
+                      <div className="flex flex-col gap-1 bg-white shadow p-4">
+                        <h2 className="font-bold  text-black">week 3:</h2>
+                        <p className="text-sm  text-black">{me.week3}</p>
+                      </div>
 
-                          {me.lecture && me.link ? (
-                            <>
-                              <div className="flex flex-col gap-1">
-                                <h2 className="text-sm text-black">Lecture</h2>
-                                <p className="text-sm text-black">
-                                  <p className="text-sm text-black">
-                                    {me.lecture}
-                                  </p>
-                                </p>
-                              </div>
-
-                              <div className="flex flex-row items-center">
-                                <div className="flex items-end gap-[18.10rem]">
-                                  <div className="flex flex-col gap-1">
-                                    <h2 className="text-sm text-black">Link</h2>
-                                    <p className="text-sm text-black">
-                                      <p className="text-sm text-black">
-                                        {me.link}
-                                      </p>
-                                    </p>
-                                  </div>
-                                </div>
-                              </div>
-                            </>
-                          ) : null}
-                        </div>
+                      <div className="flex flex-col gap-1 bg-white shadow p-4">
+                        <h2 className="font-bold  text-black">week 4:</h2>
+                        <p className="text-sm  text-black">{me.week4}</p>
                       </div>
                     </div>
                   </div>
